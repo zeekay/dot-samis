de1c9c002cbcb1a3fd7fc5dbb4c47d26bbed9c9f
zeekay <zk@monoid.io>
2015-12-29 22:46:06 -0600

    Use `fs.list_dirs`.

--- ellipsis.sh
+++ ellipsis.sh
@@ -4,7 +4,7 @@
 
 # The following hooks can be defined to customize behavior of your package:
 pkg.link() {
-    for dir in fs.list_directories; do
+    for dir in fs.list_dirs; do
         fs.link_files $dir
     done
 }
